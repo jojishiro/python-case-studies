a49517d3fe3eed73b114f654594a118b829a8d00
gzbishop <zaf.mitx@gmail.com>
2017-01-01 21:36:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,27 @@
+# Console Application
 from cx_Freeze import setup, Executable
 
 setup(name='AppName 1.9.5',
       version='1.16.9.5',
       description='Description',
       executables = [Executable('main.py')])
+#-------------------------------------------------------------------------------
+# Tkinter application Python 2.7
+import sys
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need fine tuning.
+build_exe_options = {"includes": ["Tkinter"]}
+
+# GUI applications require a different base on Windows (the default is for a
+# console application).
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+
+setup(  name = "AlgoAnalysis 1B",
+        version = "0.1",
+        description = "Exercise 1b",
+        options = {"build_exe": build_exe_options},
+        executables = [Executable("exercise_1b.py", base=base)])
